23c8bb7d97dccf37003098f35c5b3ef519f00cfe
Shoi Yamashiro <e205752@ie.u-ryukyu.ac.jp>
2021-02-01 13:39:56 +0900

    コード内容をコメント

--- src/jp/ac/uryukyu/ie/e205752/Koma.java
+++ src/jp/ac/uryukyu/ie/e205752/Koma.java
@@ -1,5 +1,11 @@
 package jp.ac.uryukyu.ie.e205752;
-
+/**
+ * オセロ一つ一つの情報をKomaクラスとして設定
+ * コマの状態を空のときはE,白の時はW,黒の時はB,赤の時はR,黄色の時はYとする。
+ * コマの状態はString型のstate変数で管理
+ * コマの状態を変更するsetStateメソット。
+ * コマの位置を取得するgetPositionメソット
+ */
 public class Koma {
     private String state;
 
