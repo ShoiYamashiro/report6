adf3217b50d6c880e3c61e94171951a3c93ba3a7
Shoi Yamashiro <e205752@ie.u-ryukyu.ac.jp>
2021-02-01 15:02:39 +0900

    オセロのコマの初期位置変更

--- src/jp/ac/uryukyu/ie/e205752/Game.java
+++ src/jp/ac/uryukyu/ie/e205752/Game.java
@@ -4,10 +4,14 @@ public class Game {
     public static void main(String[] args){
         Field field = new Field(8,8);
         field.prepare();
-        field.putKoma(4,4,"B");
-        field.putKoma(5,5,"R");
-        field.putKoma(4, 5, "W");
-        field.putKoma(5, 4, "Y");
+        field.putKoma(2,4,"R");
+        field.putKoma(3,2,"W");
+        field.putKoma(3,3,"R");
+        field.putKoma(3,4,"B");
+        field.putKoma(4,3,"W");
+        field.putKoma(4,4,"Y");
+        field.putKoma(4,5,"B");
+        field.putKoma(5,3,"Y");
         field.feature();
     }
 }
